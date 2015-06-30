5c03ad6ea4382a019592308876ed1188528ee38e
Nathaniel Jacobi <njacobi@mines.edu>
2015-06-29 20:35:54 -0600

    Added toggle buttons for the gender

--- src/skillParagraphs/GUIPanels.java
+++ src/skillParagraphs/GUIPanels.java
@@ -1,10 +1,16 @@
 package skillParagraphs;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class GUIPanels {
 	JTextField name;
@@ -12,18 +18,41 @@ public class GUIPanels {
 	Paragraph paragraph;
 	JButton newStudent;
 	
+	JPanel genderPanel;
+	JRadioButton female;
+	JRadioButton male;
 	
 	public GUIPanels(ParagraphGUI mainGUI, Paragraph paragraph) {
 		this.mainGUI = mainGUI;
 		this.paragraph = paragraph;
 		createNamePanel();
+		createGenderButton();
 		createNewStudentButton();
+
 		addListeners();
 	}
 	
 	private void createNewStudentButton() {
 		newStudent = new JButton("New Student");
 		mainGUI.add(newStudent);
+	}
+	
+	private void createGenderButton() {
+		ButtonGroup genderSelection = new ButtonGroup();
+		female = new JRadioButton("Female");
+		male = new JRadioButton("Male");
+		genderPanel = new JPanel();
+		
+		genderSelection.add(female);
+		genderSelection.add(male);
+		
+		genderPanel.add(female);
+		genderPanel.add(male);
+		
+		genderPanel.setBorder(new TitledBorder (new EtchedBorder(), "Gender"));
+		
+		//genderPanel.add(genderSelection);
+		mainGUI.add(genderPanel, BorderLayout.EAST);
 	}
 	
 	private void addListeners() {
